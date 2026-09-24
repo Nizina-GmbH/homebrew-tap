95c5422d27f2385a799c35fd4b5fa22a2a1d0b84
Mikhail Akimov <m.akimov@ctrl2go.com>
2026-09-24 12:00:16 +0300

    senderos 0.5.51 (Developer ID signed + notarized demo, telemetry v2)

--- Casks/senderos.rb
+++ Casks/senderos.rb
@@ -7,9 +7,9 @@
 # Per release, update: version + sha256 (`shasum -a 256 Senderos-<version>-arm64.dmg`).
 # The release repo is Nizina-GmbH/veyloq-releases.
 cask "senderos" do
-  version "0.5.50"
+  version "0.5.51"
   # shasum -a 256 Senderos-<version>-arm64.dmg  (fill per release)
-  sha256 "216f5410e6bed89c12fed0a2f65ae7c2bcc97db94272a3147e5b4f3e340facf2"
+  sha256 "aed1e5f78d860d59f486ccf889c13d630c1420c3dbb9390dde9f2aa87bbd12f1"
 
   url "https://github.com/Nizina-GmbH/veyloq-releases/releases/download/v#{version}/Senderos-#{version}-arm64.dmg"
   name "Senderos"
